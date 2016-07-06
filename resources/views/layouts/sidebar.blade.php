5c1f832136de824bb2b73f4f2b1cafcfe94efd0d
root <root@bruno>
2016-07-06 14:39:50 -0300

    Teste

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -11,7 +11,21 @@
             <a href="{{ url('/invoice') }}"><i class="fa fa-fw fa-table"></i> Notas Fiscais</a>
         </li>
         <li>
-            <a href="{{ url('/home') }}"><i class="fa fa-fw fa-bar-chart-o"></i> Controle de Notas</a>
+            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-bar-chart-o"></i> Controle de Notas <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="demo" class="collapse">
+                <li>
+                    <a href="{{ url('/unit') }}">Notas Escanear</a>
+                </li>
+                <li>
+                    <a href="{{ url('/bank') }}">Notas a Pagar</a>
+                </li>
+                <li>
+                    <a href="{{ url('/bank') }}">Confirmar Pagamento</a>
+                </li>
+                <li>
+                    <a href="{{ url('/bank') }}">Notas Pagas</a>
+                </li>
+            </ul>
         </li>
         <li>
             <a href="{{ url('/home') }}"><i class="fa fa-fw fa-file"></i> Emissão de DARF</a>
@@ -20,8 +34,8 @@
             <a href="{{ url('/home') }}"><i class="fa fa-fw fa-car"></i> Nota de Combustível</a>
         </li>
         <li>
-            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-wrench"></i> Configurações <i class="fa fa-fw fa-caret-down"></i></a>
-            <ul id="demo" class="collapse">
+            <a href="javascript:;" data-toggle="collapse" data-target="#config"><i class="fa fa-fw fa-wrench"></i> Configurações <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="config" class="collapse">
                 <li>
                     <a href="{{ url('/unit') }}">Unidades</a>
                 </li>
